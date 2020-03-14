178485c1fe0f0fdd38a3fdbcd346620f1d6e9eb2
josephttran <42635012+josephttran@users.noreply.github.com>
2020-03-13 22:38:23 -0700

    Fetch data during SSR

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Link from 'next/link';
+import axios from 'axios';
 
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
-function App() {
+function App({data}) {
+  console.log(data);
+
   return (
     <Layout>
-      <Link href='/submit'>Here's how to submit your work.</Link>
+      <Link href='/submit'><a>Here's how to submit your work.</a></Link>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  try {
+    const res = await axios.get('http://localhost:3000/checklist.json');
+    const data = await res.data;
+    return {
+      props: {data: data}
+    }    
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 export default App
